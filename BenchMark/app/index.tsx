49be6620dabae444357d1841efffebd93472e63d
Kevin Quah <kevinq24012@gmail.com>
2025-05-27 11:56:34 +0800

    Split SignIn & SignUp Views

--- BenchMark/app/index.tsx
+++ BenchMark/app/index.tsx
@@ -1,29 +1,20 @@
 import { useState } from "react";
-import { KeyboardAvoidingView, StyleSheet, View, Button, ActivityIndicator } from "react-native";
+import { Image } from "expo-image";
+import { KeyboardAvoidingView, StyleSheet, View, TouchableOpacity, ActivityIndicator, Text } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
 import { FirebaseError } from "firebase/app";
 import React from "react";
 
 const LoginScreen = () => {
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
+    const [signup, setSignup] = useState(false);
 
-    const signUp = async () => {
-        setLoading(true);
-        try {
-            await auth().createUserWithEmailAndPassword(email, password);
-            alert("Check your emails!");
-        } catch (e: any) {
-            const err = e as FirebaseError;
-            alert("Registration Failed: " + err.message);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    const signIn = async () => {
+    const signIn = async (email: string, password: string) => {
         setLoading(true);
         try {
             await auth().signInWithEmailAndPassword(email, password);
@@ -33,12 +24,32 @@ const LoginScreen = () => {
         } finally {
             setLoading(false);
         }
-
     }
 
-    return (
-        <View style={styles.container}>
-            <KeyboardAvoidingView behavior="padding">
+    const signUp = async (username: string, email: string, password: string) => {
+        setLoading(true);
+        try {
+            const userCredentials = await auth().createUserWithEmailAndPassword(email, password);
+            await firestore().collection("users").doc(userCredentials.user.uid).set({username: username, email: email});
+            return userCredentials.user;
+        } catch (e: any) {
+            const err = e as FirebaseError;
+            alert("Registration Failed: " + err.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const SignUpView = () => {
+        return (
+            <KeyboardAvoidingView behavior = "padding">
+                <TextInput
+                    style={styles.input}
+                    value={username}
+                    onChangeText={setUsername}
+                    autoCapitalize="none"
+                    placeholder="Username"
+                />
                 <TextInput
                     style={styles.input}
                     value={email}
@@ -52,17 +63,66 @@ const LoginScreen = () => {
                     value={password}
                     onChangeText={setPassword}
                     secureTextEntry
-                    placeholder="password"
+                    placeholder="Password"
                 />
                 {loading ? (
                     <ActivityIndicator size={'small'} style={{margin: 28}} />
                 ) : (
                     <>
-                        <Button onPress={signIn} title="Login" />
-                        <Button onPress={signUp} title="Create Account" />
+                        <TouchableOpacity style={styles.submitContainer} onPress={() => signUp(username, email, password)}>
+                            <Text style={styles.submitText}> Create Account </Text>
+                        </TouchableOpacity>
+                        <Text style={styles.changeText}> Already have an account? </Text>
+                        <TouchableOpacity style={styles.changeContainer} onPress={() => setSignup(false)}>
+                            <Text style={styles.changeButton}> Login </Text>
+                        </TouchableOpacity>
                     </>
                 )}
             </KeyboardAvoidingView>
+        );
+    };
+
+    const SignInView = () => {
+        return (
+            <KeyboardAvoidingView behavior = "padding">
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                    placeholder="Email"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                    placeholder="Password"
+                />
+                {loading ? (
+                    <ActivityIndicator size={'small'} style={{margin: 28}} />
+                ) : (
+                    <>
+                        <TouchableOpacity style={styles.submitContainer} onPress={() => signIn(email, password)}>
+                            <Text style={styles.submitText}> Login </Text>
+                        </TouchableOpacity>
+                        <Text style={styles.changeText}> Don't have an account? </Text>
+                        <TouchableOpacity style={styles.changeContainer} onPress={() => setSignup(true)}>
+                            <Text style={styles.changeButton}> Create Account </Text>
+                        </TouchableOpacity>
+                    </>
+                )}
+            </KeyboardAvoidingView>
+        );
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image source={require("@/assets/images/BenchMarkLogo.png")} style={styles.logo} />
+            </View>
+            {signup ? SignUpView() : SignInView()}
         </View>
     );
 };
@@ -74,13 +134,66 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     },
 
+    logoContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        marginBottom: 0
+    },
+
+    logo: {
+      height: 300,
+      width: 300
+    },
+
     input: {
-        marginVertical: 4,
+        marginVertical: 8,
         height: 50,
         borderWidth: 1,
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: "#fff"
+        borderColor: "#ddd",
+        borderRadius: 8,
+        padding: 12,
+        backgroundColor: "#fff",
+        fontSize: 16
+    },
+
+    submitContainer: {
+        backgroundColor: "#007AFF",
+        borderRadius: 8,
+        padding: 16,
+        marginVertical: 12,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+
+    submitText: {
+        color: "#FFFFFF",
+        fontSize: 16,
+        fontWeight: "600"
+    },
+
+    changeContainer: {
+        alignItems: "center",
+        padding: 12,
+        marginTop: 8
+    },
+
+    changeText: {
+        color: "#666",
+        fontSize: 14,
+        textAlign: "center",
+        marginTop: 20
+    },
+
+    changeButton: {
+        color: "#007AFF"
     }
 });
 
